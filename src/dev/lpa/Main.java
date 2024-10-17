8bf5d564d3df53c953c98c0eefa4efc74f9b538c
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-17 14:11:53 +0200

    matcher.find() starts where matcher left of, incremental

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String sentence = "I like motorcycles.";
+    String sentence = "I like B.M.W. motorcycles.";
     boolean matched = Pattern.matches("[A-z].*[.]", sentence);
     System.out.println(matched + ": " + sentence);
 
-    Pattern firstPattern = Pattern.compile("[A-Z].*[.]");
+    Pattern firstPattern = Pattern.compile("[A-Z].*?[.]"); // ? shortest possible solution
     var matcher = firstPattern.matcher(sentence);
     matched = matcher.matches();
     System.out.println(matched + ": " + sentence + "\n");
@@ -18,7 +18,15 @@ public class Main {
     System.out.println("sentence.length: " + sentence.length());
     System.out.println("Matched Ending Index: " + matcher.end() + "\n");
 
-    System.out.println(matcher.lookingAt() + ": " + sentence);  // doesn't match entire string
+    System.out.println(matcher.lookingAt() + ": " + sentence);  // doesn't match entire string,
+    // starts with beginning, but open end
     System.out.println("Matched Ending Index: " + matcher.end() + "\n");
+    System.out.println("Matched on : " + sentence.substring(0, matcher.end()) + "\n");
+
+    matcher.reset();  // since find starts where the matcher ended before
+    System.out.println(matcher.find() + ": " + sentence);
+    System.out.println("Matched Ending Index: " + matcher.end());
+    System.out.println("Matched on : " +
+      sentence.substring(matcher.start(), matcher.end()));
   }
 }
