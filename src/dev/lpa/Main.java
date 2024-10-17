abcba902660332642da7038a104faf42cb8206e3
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-17 13:12:57 +0200

    show Pattern.matches

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,14 @@
 package dev.lpa;
 
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    String sentence = "I like motorcycles.";
+    boolean matched = Pattern.matches("[A-z].*[.]", sentence);
+    System.out.println(matched + ": " + sentence);
 
   }
 }
